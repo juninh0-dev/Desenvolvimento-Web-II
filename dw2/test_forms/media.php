5edbed6bb4381475cfa387782e64a5d29b7d61b5
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-09-22 12:43:58 -0300

    Criei um arquivo PHP

--- dw2/test_forms/media.php
+++ dw2/test_forms/media.php
@@ -1,13 +1,19 @@
 <?php
-    $nota1 = "nota1";
+    $nota1 = $_POST["nota1"];
+    $nota2 = $_POST["nota2"];
+    $nota3 = $_POST["nota3"];
 
-    if (isset($_POST["nota1"])){
-        
+    if (!isset($nota1)){
+        echo "<p>Nota 3 não pode estar vazia</p>";
     }
-    if (isset($_POST["nota2"])){
+    if (!isset($nota2)){
+        echo "<p>Nota 2 não pode estar vazia</p>";
+    }
+    if (!isset($nota3)){
+        echo "<p>Nota 3 não pode estar vazia</p>";
+    }
 
-    }
-    if (isset($_POST["nota2"])){
+    $media = ($nota1 + $nota2 + $nota3)/3;
 
-    }
+    echo "<p>Sua média é de <strong>$media</strong> pontos.</p>";
 ?>
